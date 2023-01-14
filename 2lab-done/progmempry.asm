2918024370693c4e455c66632c4df4155abb8431
allicen <ellena.hella@yandex.ru>
2023-01-14 18:59:13 +0300

    Доработка программы

--- 2lab-done/progmempry.asm
+++ 2lab-done/progmempry.asm
@@ -38,111 +38,113 @@ rjmp RESET ; Reset Handler
 .org 0x15
 RESET:
 //init stack pointer
-// ldi Acc0, LOW(RAMEND)
-// out SPL, Acc0
-// ldi Acc0, HIGH(RAMEND)
-// out SPH, Acc0
-//init SFR (special function reg)
-// sbi DDRB, LED
+
+ldi r18, 0x00 // загрузка константы в регистр
+// X, Z - сдвоенные регистры, основное назначение - косвенна€ адресаци€
+ldi ZL, LOW(DataByte*2) // LOW - вз€ть младший байт слова, 2 - 2 байта в пам€ти, кажд адрес содержит 2 байта (0x100 * 2 = 0x200)
+ldi ZH, HIGH(DataByte*2) // HIGH - вз€ть старший байт слова
+// (1) «апись по адресу 100
+ldi XL, LOW(0x100)
+ldi XH, HIGH(0x100)
 
 //Interrupt Enable
 // sei
 
 //Main programm
 
-
-ldi r18, 0x00
-// X, Z - сдвоенные регистры, основное назначение - косвенна€ адресаци€
-LDI ZL, LOW(DataByte*2) // LOW - вз€ть младший байт слова, 2 - 2 байта в пам€ти, кажд адрес содержит 2 байта (0x100 * 2 = 0x200)
-LDI ZH, HIGH(DataByte*2) // HIGH - вз€ть старший байт слова
-ldi XL, LOW(0x0100)
-ldi XH, HIGH(0x0100)
-
 ///SRAM
+// (1) ƒозаписать все слова из словар€ по адресу 100
 Write_SRAM:
-LPM r2,Z
-st X,r2 // косвенна€ адресаци€
-inc XL
-inc r18
-inc ZL
-cpi r18,10 // сравнение регистра
-brne Write_SRAM
+	lpm R2,Z
+	st X,R2 // косвенна€ адресаци€, R2 в X
+	inc XL // инкремент
+	inc R18
+	inc ZL
+	cpi R18,10 // сравнение регистра
+	brne Write_SRAM // Branch if Not Equal
 
 ///Flash Programm
-LDI ZL, LOW(DataByte*2)
-LDI ZH, HIGH(DataByte*2)
+ldi ZL, LOW(DataByte*2)
+ldi ZH, HIGH(DataByte*2)
 
+// (2) «аписать в пам€ть 0x50
+ldi R17, LOW(0x50)
+ldi R18, HIGH(0x50)
+ldi R20, 0x00
+lpm
 
-LDI R17, LOW(0x50)
-LDI R18, HIGH(0x50)
-ldi r20, 0x00
-LPM
+loop:
+	rcall Write_EEpr
+	rcall Delay
+	rjmp loop
+	
+
 Write_EEpr:
-LPM r2,Z
-MOV r16,r2
-rcall EEPROM_write
-INC R17
-inc ZL
-inc r20
-cpi r20,10
-brne Write_EEpr
+	lpm R2,Z // пам€ть программ, с пам€тью программ работает только Z
+	mov R16,R2 // перемещение из регистра в регистр
+	//rcall EEPROM_write // вызов подпрограммы
+	inc R17 // инкремент
+	inc ZL
+	inc R20
+	cpi R20,10 // сравнение регистра с 10
+	brne Write_EEpr
+	ret
 
 
-LPM // без аргументов - берет из Z и кладет в R0
+lpm // без аргументов - берет из Z и кладет в R0
 ///EEPROM DATA - энергонезависима€ пам€ть
-LPM r2,Z // «агрузка программной пам€ти
-LDI R17, LOW(0x50)
-LDI R18, HIGH(0x50)
+lpm R2,Z // «агрузка программной пам€ти
+ldi R17, LOW(0x50)
+ldi R18, HIGH(0x50)
 
-rcall EEPROM_write
+//rcall EEPROM_write // вызов подпрограммы
 
 
 //SubProgamm
-Delay:
-nop
-nop
-
-ret
-
 //  од из документации 
 EEPROM_write:
-; Wait for completion of previous write
-sbic EECR,EEWE // ждет окончани€ записи, анализ флага EEWE в регистре EECR
-rjmp EEPROM_write
-; Set up address (r18:r17) in address register
-out EEARH, r18
-out EEARL, r17
-; Write data (r16) to Data Register
-out EEDR,r16
-; Write logical one to EEMWE
-sbi EECR,EEMWE
-; Start eeprom write by setting EEWE
-sbi EECR,EEWE
-ret
+	; Wait for completion of previous write
+	sbic EECR,EEWE // ждет окончани€ записи, анализ флага EEWE в регистре EECR
+	rjmp EEPROM_write
+	; Set up address (r18:r17) in address register
+	out EEARH, r18
+	out EEARL, r17
+	; Write data (r16) to Data Register
+	out EEDR,r16
+	; Write logical one to EEMWE
+	sbi EECR,EEMWE // установить в регистр ввода-вывода 1
+	; Start eeprom write by setting EEWE
+	sbi EECR,EEWE
+	ret
 
 //  од из документации 
 EEPROM_read:
-; Wait for completion of previous write
-sbic EECR,EEWE
-rjmp EEPROM_read
-; Set up address (r18:r17) in Address Register
-out EEARH, r18
-out EEARL, r17
-; Start eeprom read by writing EERE
-sbi EECR,EERE
-; Read data from Data Register
-in r16,EEDR
-ret
+	; Wait for completion of previous write
+	sbic EECR,EEWE
+	rjmp EEPROM_read
+	; Set up address (r18:r17) in Address Register
+	out EEARH, r18
+	out EEARL, r17
+	; Start eeprom read by writing EERE
+	sbi EECR,EERE
+	; Read data from Data Register
+	in r16,EEDR
+	ret
+
+Delay:
+	nop
+	nop
+	ret // выход из подпрограммы
 
 //Interrupt Routines
 EXT_INT0:
-push Acc0
-push Acc1
+	push Acc0 // положить в стек
+	push Acc1
 
-pop Acc0
-pop Acc1
+	pop Acc1 // вз€ть из стека
+	pop Acc0
 
-reti
+	reti // выход из прерывани€
 
 //Dat
 .org 0x100 // DataByte складываем по адресу 0x100
